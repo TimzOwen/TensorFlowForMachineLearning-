e081f20039b77584de21582caec20c8f77b8051e
Timz Owen <ktimothy@kabarak.ac.ke>
2020-01-07 14:08:56 +0300

    Update Training01.py
    updated training, plotting and testing model

--- Training01.py
+++ Training01.py
@@ -31,5 +31,40 @@ for i, c in enumerate(celsius_q):
 15.0 degrees in celsius = 59.0 degree in fahrenheit
 22.0 degrees in celsius = 72.0 degree in fahrenheit
 38.0 degrees in celsius = 100.0 degree in fahrenheit
+# define the exact layers you want your model to train on
+10 = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([10])
+
+model = tf.keras.Sequential(
+    [
+     tf.keras.layers.Dense(units=1, input_shapes=[1])
+    ]
+)
+# compile the trained model for loss and gain gauging
+model.compile(loss="mean_squared_error",
+              optimizer= tf.keras.optimizers.Adam(0.1))
+
+# provide how many times the model should train on 
+history= model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+print("finished Training model")
+
+# output
+# finished Training model
+
+# plot the training rate against the data
+import matplotlib.pyplot as plt
+plt.xlabel('epoch number')
+plt.ylabel('Loss magnitude')
+plt.plot(history.history['loss'])
+
+print(model.predict([100]))
+#outputs
+#[[211.30353]] close to exact 212
+
+print("This are the layers variables: {}".format(model.get_weights()))
+
+# prints the labels(data input in arrays form)
+# This are the layers variables: [array([[1.8251822]], dtype=float32), array([28.785307], dtype=float32)]
+
 
 
